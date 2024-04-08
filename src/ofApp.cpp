3d5774c3647aebfa53c7725a418b4ec9a865a9a8
Jose-A-Gonzalez <jose.gonzalez148@upr.edu>
2024-04-08 05:14:14 -0300

    added the ability to control the recursions with levels, using the right or left arrow. Also, the color will change for each recursion.Added the ability to see the level of each recursion by pressing the letter d. Also added brief instructions on how to change levels.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -7,7 +7,7 @@ void ofApp::setup() {
     cam.setUpAxis({0,0,1});
     cam.setDistance(200);
     text.load("Fonts/fractalFont.otf",40);
-    dataText.load("Fonts/fractalFont.otf",15);
+    dataText.load("Fonts/fractalFont.otf",18);
 }
 
 //--------------------------------------------------------------
@@ -18,46 +18,87 @@ void ofApp::update() {
 void ofApp::draw() {
     ofBackgroundGradient(ofColor(65), ofColor(0), OF_GRADIENT_BAR);
 
-    text.drawString("(Insert Fractal Name Here)",25,60);
+   dataText.drawString("Press the right arrow to level up the recursion",1025,40);
+   dataText.drawString("Press the right arrow to level up the recursion",1025,80);
 
     ofNoFill();
     switch (mode) {
     case '1': {
         // Circle
+        text.drawString("Circle Fractal",25,60);
         float r = 0.31 * ofGetHeight();
         angle += 0.01;
-        drawMode1(ofGetWidth() / 2, ofGetHeight() / 2, r, 3);
+        drawMode1(ofGetWidth() / 2, ofGetHeight() / 2, r, circleLevels, ofColor (color));
     } break;
     case '2': {
         // Tree
+         text.drawString("Tree Fractal",25,60);
         float length = 0.31 * ofGetHeight();
-        drawMode2(ofGetWidth() / 2, ofGetHeight() - 20, 10, length, 1.5 * PI);
+        drawMode2(ofGetWidth() / 2, ofGetHeight() - 20, treeLevels, length, 1.5 * PI, ofColor (color));
     } break;
     case '3': {
         // Sierpinski Triangle
+         text.drawString("Sierpinksi Triange Fractal",25,60);
         float size = 0.88 * ofGetHeight();
-        drawMode3((ofGetWidth() - size) / 2, ofGetHeight() / 2 - 0.4 * size, size, 7);
+        drawMode3((ofGetWidth() - size) / 2, ofGetHeight() / 2 - 0.4 * size, size, triangleLevels, ofColor (color));
     } break;
     case '4': {
         // Barnsley Fern
-        drawMode4(0, 0, 10 * 1000);
+         text.drawString("Barnsley Fern Fractal",25,60);
+        drawMode4(0, 0, barnsleyLevels * 1000);
     }    break;
     case '5': {
         // Koch SnowFlake
-        SnowFlake().draw();
-    }   break;
+         text.drawString("Koch SnowFlake Fractal",25,60);
+         ofSetColor(color);
+
+        ofColor Color1 = ofColor(255, 0, 0);
+        ofColor Color2 = ofColor(0, 255,0);
+        ofColor Color3 = ofColor(0,0,255);
+    
+        SnowFlake().draw(snowflakeLevels,new SnowFlake(p1,p2), Color1);
+        SnowFlake().draw(snowflakeLevels, new SnowFlake(p2, p3),Color2);
+        SnowFlake().draw(snowflakeLevels, new SnowFlake(p3, p1), Color3);
+    }   break; 
     case '6': {
         //3d Fractal
-        Fractal3D(&cam).draw(map<string, float> {{"n", 3}, {"scale", 100}});
+         text.drawString("3d Fractal",25,60);
+        Fractal3D(&cam).draw(map<string, float> {{"n", fractal3DLevels}, {"scale", 100}});
     }   break;
     }
-}
 
-void ofApp::drawMode1(float x, float y, float r, int n) {
+
+    if (showInfo) {
+        string info;
+        info += " 1. Circle Level: " + to_string(circleLevels) + "\n\n";
+        info += " 2. Tree Level: " + to_string(treeLevels) + "\n\n";
+        info += " 3. Sierpinski Triangle Level: " + to_string(triangleLevels) + "\n]\n";
+        info += " 4. Barnsley Fern Level: " + to_string(barnsleyLevels) + "\n\n";
+        info += " 5. Koch SnowFlake Level : " + to_string(snowflakeLevels) + "\n\n";
+        info += " 6. 3D Fractal Level : " + to_string(fractal3DLevels) + "\n\n";
+
+        ofSetColor(255);
+        dataText.drawString(info, 25, 140);
+    }
+    ofSetColor(255);
+} 
+
+void ofApp::drawMode1(float x, float y, float r, int n,ofColor color){
     if (n == 0) return;
 
     int delta = r * 0.35;
+    ofSetColor(color);
     ofDrawCircle(x, y, r);
+
+    ofColor colors[] = {
+        ofColor(255, 0, 0),   // Red
+        ofColor(0, 255, 0),   // Green
+        ofColor(0, 0, 255),   // Blue
+        ofColor(255, 255, 0), // Yellow
+        // Add more colors as needed
+    };
+    
+    ofColor nextColor = colors[n % (sizeof(colors) / sizeof(colors[0]))];
 
     float angle1 = angle;
     float angle2 = PI / 3 + angle;
@@ -65,39 +106,52 @@ void ofApp::drawMode1(float x, float y, float r, int n) {
     float angle4 = 2 * PI / 3 + angle;
     float angle5 = 4 * PI / 3 + angle;
     float angle6 = 5 * PI / 3 + angle;
-    drawMode1(x + r * cos(angle1), y + r * sin(angle1), delta, n - 1);
-    drawMode1(x + r * cos(angle2), y + r * sin(angle2), delta, n - 1);
-    drawMode1(x + r * cos(angle3), y + r * sin(angle3), delta, n - 1);
-    drawMode1(x + r * cos(angle4), y + r * sin(angle4), delta, n - 1);
-    drawMode1(x + r * cos(angle5), y + r * sin(angle5), delta, n - 1);
-    drawMode1(x + r * cos(angle6), y + r * sin(angle6), delta, n - 1);
+    drawMode1(x + r * cos(angle1), y + r * sin(angle1), delta, n - 1,nextColor);
+    drawMode1(x + r * cos(angle2), y + r * sin(angle2), delta, n - 1,nextColor);
+    drawMode1(x + r * cos(angle3), y + r * sin(angle3), delta, n - 1,nextColor);
+    drawMode1(x + r * cos(angle4), y + r * sin(angle4), delta, n - 1,nextColor);
+    drawMode1(x + r * cos(angle5), y + r * sin(angle5), delta, n - 1,nextColor);
+    drawMode1(x + r * cos(angle6), y + r * sin(angle6), delta, n - 1,nextColor);
 }
 
-void ofApp::drawMode2(float x, float y, int n, float length, float rad) {
+void ofApp::drawMode2(float x, float y, int n, float length, float rad,ofColor color) {
     if (n == 0) return;
 
+    ofSetColor(color);
     float x2 = x + length * cos(rad);
     float y2 = y + length * sin(rad);
 
     ofDrawLine(x, y, x2, y2);
-
-    drawMode2(x2, y2, n - 1, 0.7 * length, rad + 0.2 * PI);
-    drawMode2(x2, y2, n - 1, 0.7 * length, rad - 0.2 * PI);
+    ofColor colors[] = {
+        ofColor(0, 255, 0),     // Green
+        ofColor(128, 0, 128),   // Purple
+        ofColor(165, 42, 42), 
+        // Add more colors as needed
+    };
+    ofColor nextColor = colors[n % (sizeof(colors) / sizeof(colors[0]))];
+    drawMode2(x2, y2, n - 1, 0.7 * length, rad + 0.2 * PI,nextColor);
+    drawMode2(x2, y2, n - 1, 0.7 * length, rad - 0.2 * PI,nextColor);
 }
 
-void ofApp::drawMode3(float x, float y, float size, int n) {
+void ofApp::drawMode3(float x, float y, float size, int n, ofColor color) {
     if (n == 0) {
         return;
     }
-
     ofPoint a(x, y);
     ofPoint b(x + size, y);
     ofPoint c(x + size / 2, y + ((sqrt(3) * size) / 2));
 
+    ofSetColor(color);
     ofDrawTriangle(a, b, c);
-
-    drawMode3(x, y, size / 2, n - 1);
-    drawMode3((a.x + b.x) / 2, (a.y + b.y) / 2, size / 2, n - 1);
+     ofColor colors[] = {
+        ofColor(0, 255, 0),     // Green
+        ofColor(128, 0, 128),   // Purple
+        ofColor(165, 42, 42), 
+        // Add more colors as needed
+    };
+    ofColor nextColor = colors[n % (sizeof(colors) / sizeof(colors[0]))];
+    drawMode3(x, y, size / 2, n - 1,nextColor);
+    drawMode3((a.x + b.x) / 2, (a.y + b.y) / 2, size / 2, n - 1,nextColor);
 }
 
 void ofApp::drawMode4(float x, float y, float n) {
@@ -109,9 +163,21 @@ void ofApp::drawMode4(float x, float y, float n) {
     float py = ofMap(y, 0, 9.9983, ofGetHeight(), 0);
 
     ofFill();
-    ofSetColor(ofColor::green);
+    ofSetColor(ofColor::green); // Set color for this level
     ofDrawCircle(px, py, 0.6);
-    ofSetColor(ofColor::white);
+    ofSetColor(ofColor::white); // Reset color to white for consistency
+
+    // Define an array of colors for each recursion level
+    // ofColor colors[] = {
+    //     ofColor(255, 0, 0),   // Red
+    //     ofColor(0, 255, 0),   // Green
+    //     ofColor(0, 0, 255),   // Blue
+    //     ofColor(255, 255, 0), // Yellow
+    //     // Add more colors as needed
+    // };
+    
+    // // Get the color for the next recursion level
+    // ofColor nextColor = colors[(int)n % (sizeof(colors) / sizeof(colors[0]))];
 
     if (r < 0.01)
         drawMode4(0, 0.16 * y, n - 1);
@@ -125,12 +191,47 @@ void ofApp::drawMode4(float x, float y, float n) {
     else
         drawMode4(-0.15 * x + 0.28 * y, 0.26 * x + 0.24 * y + 0.44, n - 1);
 }
+
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key) {
     if (key >= '1' && key <= '6'){
         mode = key;
     }
+    if (key == OF_KEY_LEFT) {
+        // Decrease depth of recursion
+        if (mode == '1' && circleLevels > 1)
+            circleLevels--;
+        else if (mode == '2' && treeLevels > 1)
+            treeLevels--;
+        else if (mode == '3' && triangleLevels > 1)
+           triangleLevels--;
+        else if (mode == '4' && barnsleyLevels > 5)
+            barnsleyLevels--;
+        else if (mode == '5' && snowflakeLevels > 1)
+            snowflakeLevels--;
+        else if (mode == '6' && fractal3DLevels > 0)
+            fractal3DLevels--;
+    } else if (key == OF_KEY_RIGHT) {
+        // Increase depth of recursion
+        if (mode == '1' && circleLevels < 5)
+            circleLevels++;
+        else if (mode == '2' && treeLevels < 15)
+            treeLevels++;
+        else if (mode == '3' && triangleLevels < 10)
+            triangleLevels++;
+        else if (mode == '4' && barnsleyLevels < 50)
+            barnsleyLevels++;
+        else if (mode == '5' && snowflakeLevels < 6)
+            snowflakeLevels++;
+        else if (mode == '6' && fractal3DLevels < 9)
+            fractal3DLevels++;
+    }
+    if (key == 'd') {
+        showInfo = !showInfo;
+    }
 }
+
+
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key) {
--- src/ofApp.h
+++ src/ofApp.h
@@ -7,12 +7,28 @@
 class ofApp : public ofBaseApp {
   private:
     char mode = '1';
+    int levels = 1;
+    int circleLevels = 1;
+    int treeLevels = 1;
+    int triangleLevels = 1;
+    int barnsleyLevels = 5;
+    int snowflakeLevels = 1;
+    int fractal3DLevels= 0;
+    float size = 0.74 * ofGetHeight(); 
+    bool showInfo = false;
     float angle = 0;
+
+
 
   public:
     void setup();
     void update();
     void draw();
+    
+    
+    glm::vec2 p1 = {(ofGetWidth() - size) / 2, (ofGetHeight() - size * sin(PI / 3)) / 2 + 0.15 * size};
+    glm::vec2 p2 = {(ofGetWidth() + size) / 2, (ofGetHeight() - size * sin(PI / 3)) / 2 + 0.15 * size};
+    glm::vec2 p3 = {ofGetWidth() / 2, (ofGetHeight() + size * sin(PI / 3)) / 2 + 0.15 * size}; 
 
     void keyPressed(int key);
     void keyReleased(int key);
@@ -26,12 +42,13 @@ class ofApp : public ofBaseApp {
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
 
-    void drawMode1(float x, float y, float r, int n);
-    void drawMode2(float x, float y, int n, float length, float rad);
-    void drawMode3(float x, float y, float size, int n);
+    void drawMode1(float x, float y, float r, int n, ofColor color);
+    void drawMode2(float x, float y, int n, float length, float rad,ofColor color);
+    void drawMode3(float x, float y, float size, int n, ofColor color);
     void drawMode4(float x, float y, float n);
 
     ofEasyCam cam;
     ofTrueTypeFont text;
     ofTrueTypeFont dataText;
+    ofColor color;
 };

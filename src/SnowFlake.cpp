24127fd94d54c1af26645056bfe86b6132f2f3d7
Jose-A-Gonzalez <jose.gonzalez148@upr.edu>
2024-04-08 05:11:03 -0300

    Added colors in the parameters of Snowflakes

--- src/SnowFlake.cpp
+++ src/SnowFlake.cpp
@@ -8,26 +8,39 @@ SnowFlake::SnowFlake(glm::vec2 start, glm::vec2 end) {
     this->end = end;
 }
 
-void SnowFlake::draw() {
-    float size = 0.74 * ofGetHeight();
+// void SnowFlake::draw() {
+//     float size = 0.74 * ofGetHeight();
 
-    glm::vec2 p1 = {(ofGetWidth() - size) / 2, (ofGetHeight() - size * sin(PI / 3)) / 2 + 0.15 * size};
-    glm::vec2 p2 = {(ofGetWidth() + size) / 2, (ofGetHeight() - size * sin(PI / 3)) / 2 + 0.15 * size};
-    glm::vec2 p3 = {ofGetWidth() / 2, (ofGetHeight() + size * sin(PI / 3)) / 2 + 0.15 * size};
+//     glm::vec2 p1 = {(ofGetWidth() - size) / 2, (ofGetHeight() - size * sin(PI / 3)) / 2 + 0.15 * size};
+//     glm::vec2 p2 = {(ofGetWidth() + size) / 2, (ofGetHeight() - size * sin(PI / 3)) / 2 + 0.15 * size};
+//     glm::vec2 p3 = {ofGetWidth() / 2, (ofGetHeight() + size * sin(PI / 3)) / 2 + 0.15 * size};
 
-    draw(5, new SnowFlake(p1, p2));
-    draw(5, new SnowFlake(p2, p3));
-    draw(5, new SnowFlake(p3, p1));
-}
-void SnowFlake::draw(int n, SnowFlake *flake) {
-    if (n < 2)
-        ofDrawLine(flake->getStart(), flake->getEnd());
-    else {
-        draw(n - 1, new SnowFlake(flake->getA(), flake->getB()));
-        draw(n - 1, new SnowFlake(flake->getB(), flake->getC()));
-        draw(n - 1, new SnowFlake(flake->getC(), flake->getD()));
-        draw(n - 1, new SnowFlake(flake->getD(), flake->getE()));
+//     draw(1, new SnowFlake(p1, p2));
+//     draw(1, new SnowFlake(p2, p3));
+//     draw(1, new SnowFlake(p3, p1));
+// }
+void SnowFlake::draw(int n, SnowFlake *flake, ofColor color) {
+    if (n < 2){
+        if(white){
+        ofSetColor(ofColor::white);
+        white=false;
+        }
+          else {
+            ofSetColor(color);
+        }
+        ofDrawLine(flake->getStart(), flake->getEnd()); 
+       
     }
+    else { 
+       
+        ofSetColor(color);
+        draw(n - 1, new SnowFlake(flake->getA(), flake->getB()), color);
+        draw(n - 1, new SnowFlake(flake->getB(), flake->getC()), color);
+        draw(n - 1, new SnowFlake(flake->getC(), flake->getD()), color);
+        draw(n - 1, new SnowFlake(flake->getD(), flake->getE()), color);
+       
+    }
+        
     delete flake;
 }
 

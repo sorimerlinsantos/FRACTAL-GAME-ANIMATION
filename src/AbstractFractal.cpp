d423617c7adcd6ca06a29695ea22a1d42d969912
insert GitHub username <sorimerlin.santos@upr.edu>
2024-04-23 16:51:43 -0400

    PHASE 2:FIX

--- src/AbstractFractal.cpp
+++ src/AbstractFractal.cpp
@@ -1,12 +1,6 @@
 #include "AbstractFractal.h"
 
 
-// class AbstractFractal: public Fractal{
-//     private:
-//         string name;
-//         int level; 
-//     public:
-
 // de la clase de abstract fractal se va a crear el constructor de abstract fractal
 
 
@@ -14,17 +8,10 @@
 
 AbstractFractal::AbstractFractal(){
             this->name="";
-            // this->level=0;
         }
 AbstractFractal::AbstractFractal(string name){
     this->name=name;
-    // this->level=level; 
+    
 
 }
-// //getters 
-// string AbstractFractal::getName(){return name;}
-// int AbstractFractal::getLevel(){return level;}
-// //setters 
-// void AbstractFractal::setName(string name){this->name=name;}
-// void AbstractFractal::setLevel(int level){this->level=level;}
-// //
+
